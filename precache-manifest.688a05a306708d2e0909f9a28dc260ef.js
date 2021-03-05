dd2b6e15cfc47c56ad5c713adc1e2b2377029244
jprawita <jprawita@gmail.com>
2021-03-05 11:18:55 +0800

    initial commit

---
+++ precache-manifest.688a05a306708d2e0909f9a28dc260ef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d35c0a3339dd56f63e57",
+    "url": "/jp-vue-meet-build/css/about.b10f3635.css"
+  },
+  {
+    "revision": "1fb88521bf1567fef9d2",
+    "url": "/jp-vue-meet-build/css/app.21f93314.css"
+  },
+  {
+    "revision": "88e5afe34e9dc12bf661",
+    "url": "/jp-vue-meet-build/css/chunk-vendors.15bed3f6.css"
+  },
+  {
+    "revision": "dd34661bfd2cd52bf8e80ebcae6ff80d",
+    "url": "/jp-vue-meet-build/index.html"
+  },
+  {
+    "revision": "d35c0a3339dd56f63e57",
+    "url": "/jp-vue-meet-build/js/about.eb5b2dac.js"
+  },
+  {
+    "revision": "1fb88521bf1567fef9d2",
+    "url": "/jp-vue-meet-build/js/app.d2c2f8de.js"
+  },
+  {
+    "revision": "88e5afe34e9dc12bf661",
+    "url": "/jp-vue-meet-build/js/chunk-vendors.96603769.js"
+  },
+  {
+    "revision": "60f3e8f3b8de918a54ea1736f2195ade",
+    "url": "/jp-vue-meet-build/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/jp-vue-meet-build/robots.txt"
+  }
+]);
